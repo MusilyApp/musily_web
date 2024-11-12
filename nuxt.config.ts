1816d41beee3a29d13e1cef3f8376ceccdf1f664
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-11-12 19:34:45 -0300

    added: style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/eslint-module'],
-  css: ['~/assets/scss/variables.scss', '~/assets/scss/themes.scss'],
+  css: [
+    '~/assets/scss/variables.scss',
+    '~/assets/scss/themes.scss',
+    '~/assets/scss/colors.scss',
+    '~/assets/scss/layout.scss',
+    '~/assets/scss/typography.scss',
+  ],
 });

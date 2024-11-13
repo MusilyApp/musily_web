e69460baedc81016cb3ade19148a250b5bcbfcf5
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-11-13 10:35:51 -0300

    added: og:img

--- app.vue
+++ app.vue
@@ -3,6 +3,10 @@ useTheme();
 useSeoMeta({
   title: 'Musily: A great music app.',
   description: 'Enjoy your music with a great music app.',
+  ogImage: '/icons/og_img.png',
+  ogImageWidth: '1024',
+  ogImageHeight: '1024',
+  ogImageType: 'image/png',
 });
 </script>
 

fef2039f73693026a55a738febc22639d6482917
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-11-13 11:19:20 -0300

    improved: SEO

--- app.vue
+++ app.vue
@@ -18,6 +18,26 @@ useSeoMeta({
 
 <template>
   <div>
+    <Head>
+      <Link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <Link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <Link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <Link rel="manifest" href="/site.webmanifest" />
+    </Head>
     <NuxtLayout>
       <slot />
     </NuxtLayout>

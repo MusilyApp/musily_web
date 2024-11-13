9ffc229fadac1740ca9a55ca123c2bc77aa657c2
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-11-13 11:10:28 -0300

    added: new metatags

--- app.vue
+++ app.vue
@@ -2,7 +2,12 @@
 useTheme();
 useSeoMeta({
   title: 'Musily: A great music app.',
+  ogTitle: 'Musily: A great music app.',
   description: 'Enjoy your music with a great music app.',
+  ogDescription: 'Enjoy your music with a great music app.',
+  ogUrl: 'https://musily.app/',
+  ogType: 'website',
+  ogLocale: 'en_US',
   ogImage: '/icons/og_img.png',
   ogImageWidth: '1024',
   ogImageHeight: '1024',

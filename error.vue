494fd3cf0f1468aa48d34a6bbdea30418ff8e33b
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-11-17 01:17:17 -0300

    feature: custom error added

---
+++ error.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+defineProps<{
+  error: NuxtError;
+}>();
+
+const handleError = () => clearError({ redirect: '/' });
+
+definePageMeta({
+  layout: 'default',
+});
+
+useTheme();
+</script>
+
+<template>
+  <div
+    style="height: calc(100vh - 100px)"
+    class="d-flex flex-col align-center justify-center px-5"
+  >
+    <MusilyLogo width="150px" height="150px" />
+    <LyText font="body-large-600">
+      {{ error.message.split(':')[0] }}
+    </LyText>
+    <p>
+      <LyFilledButton @click="handleError"> Home Page </LyFilledButton>
+    </p>
+  </div>
+</template>
